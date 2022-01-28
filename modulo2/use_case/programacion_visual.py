1749223856397c64dddb50d896ceb4a50555e8c3
Toshio Estevez <89396289+toshio3024@users.noreply.github.com>
2022-01-28 14:38:30 +0000

    entrado al modulo2

---
+++ modulo2/use_case/programacion_visual.py
@@ -0,0 +1,13 @@
+print("digita el nombre a bsucar: ") 
+
+nombre = input()
+
+
+print("cantidad de resultado: ","3")
+
+
+print("{:<10} {:<10}".format("Nombre", "Apellido"))
+print("_"*25)
+print("{:<10} {:<10}".format("Albert", "Garcia"))
+print("marlin       vasquez")
+print("aracelis     Severino")
